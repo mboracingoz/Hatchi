debc0e119c6b91ba59a5bd06f6fb01fd47d6e639
Bora Cingöz <mboracingoz@gmail.com>
2026-04-10 16:07:19 +0300

    Added clean and sleep actions and connect them to hygiene and sleep stats

--- Core/ui/Action/CleanButton.gd
+++ Core/ui/Action/CleanButton.gd
@@ -2,19 +2,16 @@ extends Button
 
 @onready var need_system = get_tree().get_first_node_in_group("need_system")
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pressed.connect(_on_pressed)
-
 
 func _on_pressed() -> void:
 	if need_system == null:
 		return
-	
-	var happiness = need_system.needs.get("happiness")
-	if happiness != null:
-		happiness.increase(100.0)
-	
-	print("Cuddle button pressed")	
-	
+
+	var hygiene = need_system.needs.get("hygiene")
+	if hygiene == null:
+		return
+
+	hygiene.increase(25.0)
+	print("Clean button pressed")
--- Core/ui/Action/SleepButton.gd
+++ Core/ui/Action/SleepButton.gd
@@ -2,19 +2,16 @@ extends Button
 
 @onready var need_system = get_tree().get_first_node_in_group("need_system")
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pressed.connect(_on_pressed)
-
 
 func _on_pressed() -> void:
 	if need_system == null:
 		return
-	
-	var happiness = need_system.needs.get("happiness")
-	if happiness != null:
-		happiness.increase(100.0)
-	
-	print("Cuddle button pressed")	
-	
+
+	var sleep_stat = need_system.needs.get("sleep")
+	if sleep_stat == null:
+		return
+
+	sleep_stat.increase(20.0)
+	print("Sleep button pressed")
