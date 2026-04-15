52e61227470279c29a3afc0e80dd507e874c4073
Bora Cingöz <mboracingoz@gmail.com>
2026-04-15 15:14:19 +0300

    Added basic idle pulse animation for normal state

--- Core/pet/idle/PetIdleController.gd
+++ Core/pet/idle/PetIdleController.gd
@@ -8,6 +8,15 @@ class_name PetIdleController
 @export var idle_interval_max: float = 5.0
 @export var process_enabled: bool = true
 
+#TWEEN
+@export var idle_pulse_scale: float = 1.03
+@export  var idle_pulse_duration: float = 0.22
+
+var _idle_tween: Tween
+var _is_idle_playing: bool = false
+
+
+#TİMER
 var _idle_timer: float = 0.0
 var _current_idle_interval: float = 0.0
 
@@ -41,14 +50,48 @@ func _process(delta: float) -> void:
 
 
 func _can_run_idle() -> bool:
-	if pet_state_controller.current_state == PetStateController.STATE_SLEEPING:
+	if _is_idle_playing:
 		return false
 	
-	if pet_state_controller.current_state == PetStateController.STATE_CRITICAL:
+	if pet_visual == null or pet_state_controller == null:
+		return false
+	
+	if pet_state_controller.current_state != PetStateController.STATE_NORMAL:
 		return false
 	
 	return true
 
 
 func _trigger_idle() -> void:
-	print("Idle trigger fired")
+	if pet_visual == null:
+		return
+	
+	if _idle_tween != null:
+		_idle_tween.kill()
+	
+	_is_idle_playing = true
+	
+	_idle_tween = create_tween()
+	_idle_tween.set_trans(Tween.TRANS_SINE)
+	_idle_tween.set_ease(Tween.EASE_IN_OUT)
+	
+	_idle_tween.tween_property(
+		pet_visual,
+		"scale",
+		Vector2.ONE * idle_pulse_scale,
+		idle_pulse_duration
+	)
+	_idle_tween.tween_property(
+		pet_visual,
+		"scale",
+		Vector2.ONE,
+		idle_pulse_duration
+	)
+	_idle_tween.finished.connect(_on_idle_finished)
+
+func _on_idle_finished() -> void:
+	_is_idle_playing = false
+	_idle_tween = null
+	
+	if pet_visual != null:
+		pet_visual.scale = Vector2.ONE
